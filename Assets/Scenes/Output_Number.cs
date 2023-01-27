de641852cbf985fd777ecbde113ef1727a8e99d2
Jared Seymour <jarexjared@gmail.com>
2023-01-27 13:57:19 +0000

    try
    try

--- Assets/Scenes/Output_Number.cs
+++ Assets/Scenes/Output_Number.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Output_card : MonoBehaviour
+public class Output_Number : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TMP_Text Number_text;
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -15,10 +18,8 @@ public class Output_card : MonoBehaviour
         
     }
 
-    public void ShowCard ()
+    public void ShowNumber()
     {
-        
+        Number_text.text = "hello world";
     }
-    
-
 }
